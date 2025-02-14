e14aa78fd094e8e0f688870ff6b6a349c7b4d2d4
cnguyen111 <cnguyen1@wpi.edu>
2025-02-14 14:10:27 -0500

    Back to Vercel 1.2

--- src/server.js
+++ src/server.js
@@ -219,3 +219,4 @@ app.post("/api/submit", async (req, res) => {
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+export default app;
