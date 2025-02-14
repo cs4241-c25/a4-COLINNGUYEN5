9c4376ff83702905ded765f3ba52bd8aa0764478
cnguyen111 <cnguyen1@wpi.edu>
2025-02-14 13:02:14 -0500

    Back to Glitch deployment 1.0

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import express from 'vite-plugin-express'
 
 export default defineConfig({
   plugins: [
